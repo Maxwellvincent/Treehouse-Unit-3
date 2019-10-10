8ef1b703a5b7cddd4e406bae83825dd3b3d98a02
Louis Maxwell <louismaxwell@knights.ucf.edu>
2019-10-10 02:02:36 -0400

    Delete script.js

--- interactive-form-v1/JS/script.js
+++
@@ -1,6 +0,0 @@
-// Focus the form to the first text input
-$('#name').focus();
-
-// create a text field that shows when "other" option is selected from the Job role drop down menu
-$('#other-title').show();
-
